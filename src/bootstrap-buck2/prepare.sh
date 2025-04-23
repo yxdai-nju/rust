934e0fda361e07347774a854debcd06c7161f549
Yuxuan Dai <yxdai@smail.nju.edu.cn>
2025-04-23 17:58:56 +0800

    Use Reindeer's workspace support in library * Add workspace configurations in 'library/reindeer.toml' * Remove 'library/Cargo.toml', use 'library/src/Cargo.toml' instead * Use rsync to copy source files, instead of creating symlinks
    Signed-off-by: Yuxuan Dai <yxdai@smail.nju.edu.cn>

--- src/bootstrap-buck2/prepare.sh
+++ src/bootstrap-buck2/prepare.sh
@@ -1,30 +1,11 @@
 #!/bin/sh
-set -e
+set -ex
 
-# Record the path where this script is run (must be ran under '[project root]/src/bootstrap-buck2')
+# Record the path where this script is run (must be ran under '[rust]/src/bootstrap-buck2')
 BOOTSTRAP_BUCK2_PATH=$(pwd)
 
-# Function to create an absolute path symlink
-create_absolute_symlink() {
-    local target_dir="$1"
-    local source_dir="$2"
-    
-    if [ -e "$target_dir" ]; then
-        if [ -L "$target_dir" ]; then
-            rm "$target_dir"
-        else
-            echo "Error: $target_dir exists but is not a symlink"
-            exit 1
-        fi
-    fi
-    
-    ln -s "$(realpath "$source_dir")" "$target_dir"
-    echo "Created symlink: $target_dir -> $(realpath "$source_dir")"
-}
-
-# Create symlinks to standard library and compiler sources
-create_absolute_symlink "$BOOTSTRAP_BUCK2_PATH/library/src" "$BOOTSTRAP_BUCK2_PATH/../../library"
-create_absolute_symlink "$BOOTSTRAP_BUCK2_PATH/compiler/src" "$BOOTSTRAP_BUCK2_PATH/../../compiler"
+rsync --delete --archive $BOOTSTRAP_BUCK2_PATH/../../library/  $BOOTSTRAP_BUCK2_PATH/library/src
+rsync --delete --archive $BOOTSTRAP_BUCK2_PATH/../../compiler/ $BOOTSTRAP_BUCK2_PATH/compiler/src
 
 # Generate BUCK files for third-party dependencies
 echo "Generating BUCK file for internal tools..."
@@ -32,10 +13,9 @@ reindeer --third-party-dir $BOOTSTRAP_BUCK2_PATH/bootstrap/tools/third-party buc
 echo "Completed"
 
 echo "Generating BUCK file for library..."
-reindeer --third-party-dir $BOOTSTRAP_BUCK2_PATH/library buckify
+cd $BOOTSTRAP_BUCK2_PATH/library && reindeer buckify
 echo "Completed"
 
-# TODO(yxdai-nju): Support building compiler
-# echo "Generating BUCK file for compiler..."
-# reindeer --third-party-dir $BOOTSTRAP_BUCK2_PATH/compiler buckify
-# echo "Completed"
+echo "Generating BUCK file for compiler..."
+reindeer --third-party-dir $BOOTSTRAP_BUCK2_PATH/compiler buckify
+echo "Completed"
